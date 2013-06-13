8012301ca3f439aa35c942ea1011b10d4daefa27
thequbit <thequbit@gmail.com>
2013-06-13 19:42:47 -0400

    geojson api script

---
+++ web/api/geojson.php
@@ -0,0 +1,40 @@
+<?
+
+	require_once("../tools/CrimesManager.class.php");
+	$mgr = new CrimesManager();
+
+	if( isset($_GET['year']) && isset($_GET['month']) )
+	{
+		$year = $_GET['year'];
+		$month = $_GET['month'];
+		
+		$results = $mgr->getallbymonth($year,$month);
+		
+		$features = array();
+		foreach($results as $result)
+		{
+		    $latlng = array();
+			$latlng[] = $result->lat;
+			$latlng[] = $result->lng;
+			
+			$feature = (object)array('type' => 'Feature', 
+			                          'geometry' => (object)array('type' => 'Point', 'coordinates' => $latlng),
+									  'properties' => (object)array('crime' => $result->crime, 'fulladdress' => $result->fulladdress)
+									  );
+			
+			$features[] = $feature;
+		}
+		
+		$retval = (object)array('type' => 'FeatureCollection', 'features' => $features);
+		
+		echo json_encode($retval);
+	}
+	else
+	{
+		$err = (object) array('errorcode' => 1, 'errortext' => 'Invalid Input.');
+		echo json_encode($err);
+	}
+
+	exit;
+	
+?>
